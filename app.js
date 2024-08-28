c9a4640e67c098eb63508900f8ca2874eba16b32
duyvo112 <duyb2111833@student.ctu.edu.vn>
2024-08-28 21:00:07 +0700

    cai dat xu ly loi

--- app.js
+++ app.js
@@ -2,14 +2,27 @@ const express = require('express');
 const cors = require('cors');
 const app=express();
 const contactRouter = require("./app/routes/contact.route");
+const ApiError = require("./app/api-error");
 
 app.use(cors());
 app.use(express.json());
 
-// app.get("/",(req,res)=>{
-//     res.json("Hello World");
-// });
+//define Route
+app.get("/",(req,res)=>{
+    res.json("Hello World");
+});
 
 app.use("/api/contacts",contactRouter);
+
+// Handle 404 response
+app.use((req, res, next) => {
+    return next(new ApiError(404, "Resource Not Found"));
+  });
+
+app.use((error,req, res, next) => {
+    return res.status(error.statusCode || 500).json({
+        message: error.message || "Internal Server Error",
+    });
+  });
 
 module.exports = app;
