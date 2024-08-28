aacb3cb30d6c61710ec452fa2dbcc98ab06d66d9
duyvo112 <duyb2111833@student.ctu.edu.vn>
2024-08-28 20:34:01 +0700

    Dinh nghia cac route cho tai nguyen contact

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const cors = require('cors');
 const app=express();
+const contactRouter = require("./app/routes/contact.route");
 
 app.use(cors());
 app.use(express.json());
 
-app.get("/",(req,res)=>{
-    res.json("Hello World");
-});
+// app.get("/",(req,res)=>{
+//     res.json("Hello World");
+// });
+
+app.use("/api/contacts",contactRouter);
 
 module.exports = app;
